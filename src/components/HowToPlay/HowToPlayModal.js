2b0a564ee2c9981db00175f76db1c07f23db02c9
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2021-11-06 17:12:33 +0100

    update How to Play instructions

--- src/components/HowToPlay/HowToPlayModal.js
+++ src/components/HowToPlay/HowToPlayModal.js
@@ -8,19 +8,21 @@ function HowToPlayModal({ close }) {
       <div className={styles.modal}>
         <img onClick={close} src={Close} alt="close" />
         <h3>How to Play</h3>
-        <p>
+        <p style={{ marginBottom: "10px" }}>
           This game is commonly referred to as "Sliding Puzzle" and it is played
           by shifting blocks that are next to the puzzle box's vacant cell. All
           you have to do in this game is tap on the blocks that appear next to
           the vacant cell in order for those blocks to be moved into that cell.
-          <br />
+        </p>
+        <p style={{ marginBottom: "10px" }}>
           The goal of the game is to arrange all of the blocks such that the
           numbers linked to them are in ascending order (smallest to largest),
           then place the empty cell in the bottom right corner of the box to
           finish the puzzle.
-          <br />A timer is set, and your movements are counted to keep track of
-          your progress. The more you practice, the faster and better you become
-          👍
+        </p>
+        <p>
+          A timer is set, and your movements are counted to keep track of your
+          progress. The more you practice, the faster and better you become 👍
         </p>
       </div>
     </div>
